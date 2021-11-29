dc7f893d9e60764ada44bace335f4398b97e0a49
“janeqqrul” <jan.karolewski1b@gmail.com>
2021-11-29 12:26:43 +0100

    Create Crop,Negative,Rotate,Compress mutation

--- images/schema.py
+++ images/schema.py
@@ -2,6 +2,7 @@ import base64
 from io import BytesIO
 
 import graphene
+from PIL import ImageOps
 from django.core.files.base import ContentFile
 from graphene import relay
 from graphene_django.types import DjangoObjectType
@@ -51,7 +52,7 @@ class ImageResizeMutation(relay.ClientIDMutation):
 
         new_buffer = BytesIO()
 
-        resized_img.convert('RGB').save(new_buffer, format="JPEG")
+        resized_img.save(new_buffer, format="JPEG")
         resized_img_str = base64.b64encode(new_buffer.getvalue()).decode('utf-8')
         resized_img_data = base64.b64decode(resized_img_str)
 
@@ -93,7 +94,7 @@ class ImageCropMutation(relay.ClientIDMutation):
         new_buffer = BytesIO()
         crop_img = kwargs.get('pillow_img').crop(crop_box)
 
-        crop_img.convert('RGB').save(new_buffer, format="JPEG")
+        crop_img.save(new_buffer, format="JPEG")
         crop_img_str = base64.b64encode(new_buffer.getvalue()).decode('utf-8')
         crop_img_data = base64.b64decode(crop_img_str)
 
@@ -105,6 +106,98 @@ class ImageCropMutation(relay.ClientIDMutation):
         return ImageCropMutation(obj)
 
 
+class ImageRotateMutation(relay.ClientIDMutation):
+    class Input:
+        base64 = graphene.String(required=True)
+        angle = graphene.Int(required=True)
+
+    image = graphene.Field(ImageType)
+
+    @classmethod
+    @prepare_image
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        angle = kwargs.get('angle')
+
+        new_buffer = BytesIO()
+        rotate_img = kwargs.get('pillow_img').rotate(angle)
+
+        rotate_img.save(new_buffer, format="JPEG")
+        rotate_img_str = base64.b64encode(new_buffer.getvalue()).decode('utf-8')
+        rotate_img_data = base64.b64decode(rotate_img_str)
+
+        obj = Image.objects.create(
+            base64=rotate_img_str,
+            path=ContentFile(rotate_img_data, name="rotate_img.jpeg")
+        )
+
+        return ImageRotateMutation(obj)
+
+
+class ImageNegativeMutation(relay.ClientIDMutation):
+    class Input:
+        base64 = graphene.String(required=True)
+
+    image = graphene.Field(ImageType)
+
+    @classmethod
+    @prepare_image
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        new_buffer = BytesIO()
+
+        negative_img = ImageOps.invert(kwargs.get('pillow_img'))
+
+        negative_img.save(new_buffer, format="JPEG")
+        negative_img_str = base64.b64encode(new_buffer.getvalue()).decode('utf-8')
+        negative_img_data = base64.b64decode(negative_img_str)
+
+        obj = Image.objects.create(
+            base64=negative_img_str,
+            path=ContentFile(negative_img_data, name="negative_img.jpeg")
+        )
+
+        return ImageNegativeMutation(obj)
+
+
+class ImageCompressionMutation(relay.ClientIDMutation):
+    class Input:
+        base64 = graphene.String(required=True)
+        quality = graphene.Int(required=True)
+
+    image = graphene.Field(ImageType)
+
+
+    @classmethod
+    @prepare_image
+    def mutate_and_get_payload(cls, root, info, **kwargs):
+        quality = kwargs.get('quality')
+        new_buffer = BytesIO()
+        kwargs.get('pillow_img').save(
+            new_buffer,
+            format='JPEG',
+            optimize=True,
+            quality=quality
+        )
+
+        compressed_img_str = base64.b64encode(new_buffer.getvalue()).decode('utf-8')
+        compressed_img_data = base64.b64decode(compressed_img_str)
+
+        obj = Image.objects.create(
+            base64=compressed_img_str,
+            path=ContentFile(compressed_img_data, name="compressed_img.jpeg")
+        )
+
+        return ImageCompressionMutation(obj)
+
+
+
+
+
+
+
+
 class Mutation:
     resize_image = ImageResizeMutation.Field()
     crop_image = ImageCropMutation.Field()
+    rotate_image = ImageRotateMutation.Field()
+    negate_image = ImageNegativeMutation.Field()
+    compress_image = ImageCompressionMutation.Field()
